bf30fe6af598887428ebab7661a7221f035f384f
tyunh-lab <tyunhs@gmail.com>
2023-12-09 18:03:13 +0900

    計算方法の改善

--- ume8.c
+++ ume8.c
@@ -2,15 +2,26 @@
 
 int main()
 {
-    int n, m;
+    int n = 100, m = 10;
 
-    printf("mを入力\n");
-    scanf("%d", &m);
-    printf("nを入力\n");
-    scanf("%d", &n);
+    // printf("mを入力\n");
+    // scanf("%d", &m);
+    // printf("nを入力\n");
+    // scanf("%d", &n);
 
-    int total = n * (n + 1) / 2 - (m - 1) * m / 2;
-
-    printf("total = %d\n", total);
+    if (m >= 0 && n >= m)
+    {
+        // n * (n + 1) / 2 - (m - 1) * m / 2
+        //(n^2 + n - m^2 - m) / 2
+        //(n^2 - m^2 + n-m) / 2
+        //{(n + m)(n - m)+ n-m} / 2
+        //(n - m){(n + m) + 1} / 2
+        int total = (n - m) * (n + m + 1) / 2;
+        printf("total = %d\n", total);
+    }
+    else
+    {
+        printf("mとnの値が不正です\n");
+    }
     return 0;
 }
